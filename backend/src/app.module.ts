5bb2f1dd4d4bafe2440dc4cf454d89911303b11f
smartVIN <mbafc1204@icloud.com>
2025-07-14 11:29:12 +0200

    add KaiaAuthModule to app

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { LineAuthModule } from './line-auth/line-auth.module';
 import { LineBotModule } from './line_official_account/lineBot.module';
 import { GoogleAuthModule } from './google-auth/google-auth.module';
+import { KaiaAuthModule } from './kaia-auth/kaia-auth.module';
 
 @Module({
-  imports: [LineAuthModule, LineBotModule, GoogleAuthModule],
+  imports: [LineAuthModule, LineBotModule, GoogleAuthModule, KaiaAuthModule],
   controllers: [],
   providers: [],
 })
